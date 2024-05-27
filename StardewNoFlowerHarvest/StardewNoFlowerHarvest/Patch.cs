57bf912ad4bbb047a9bf6699c08fa0b9a3abaab4
Sabrina Eichenberger <54446558+seichen@users.noreply.github.com>
2024-05-27 17:19:38 -0500

    first commit

---
+++ StardewNoFlowerHarvest/StardewNoFlowerHarvest/Patch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewModdingAPI;
+using StardewValley.GameData.Crops;
+using StardewValley;
+
+namespace StopFlowerHarvests
+{
+    internal class Patch
+    {
+        public static HarvestMethod Postfix_GetHarvestMethod(HarvestMethod __result, Crop __instance)
+        {
+            if (__instance.GetData().HarvestItemId.Equals("595"))
+            {
+                return HarvestMethod.Scythe;
+            }
+            return __result;
+        }
+    }
+}
