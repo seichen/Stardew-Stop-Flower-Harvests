74d350a8b01b360e6999382fa094b2411dac6ab9
Sabrina Eichenberger <54446558+seichen@users.noreply.github.com>
2024-06-03 18:15:21 -0500

    Update Patch.cs
    Patched bug where instance did not exist

--- StardewNoFlowerHarvest/StardewNoFlowerHarvest/Patch.cs
+++ StardewNoFlowerHarvest/StardewNoFlowerHarvest/Patch.cs
@@ -13,11 +13,18 @@ namespace StopFlowerHarvests
     {
         public static HarvestMethod Postfix_GetHarvestMethod(HarvestMethod __result, Crop __instance)
         {
-            if (__instance.GetData().HarvestItemId.Equals("595"))
+            try
             {
-                return HarvestMethod.Scythe;
+                if (__instance.GetData().HarvestItemId.Equals("595"))
+                {
+                    return HarvestMethod.Scythe;
+                }
+                return __result;
             }
-            return __result;
+            catch (Exception)
+            {
+                return __result;
+            }
         }
     }
 }
